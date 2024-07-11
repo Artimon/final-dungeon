0b5f4b87d319b5fcd523b4592c7c6249e1013bfd
Artimon <webmaster@pad-soft.de>
2024-07-11 21:07:19 +0200

    Adds enemy targeting.

--- Scripts/Battle/UserInterface/ComponentActionMenu.cs
+++ Scripts/Battle/UserInterface/ComponentActionMenu.cs
@@ -5,6 +5,12 @@ namespace FinalDungeon.Battle.UserInterface;
 
 [GlobalClass]
 public partial class ComponentActionMenu : Control {
+	public enum Direction {
+		Left,
+		Right,
+		// Future directions can be added here, e.g., Up, Down
+	}
+
 	public static ComponentActionMenu instance;
 
 	[Export]
@@ -16,6 +22,8 @@ public partial class ComponentActionMenu : Control {
 	public ActorHero _actor;
 
 	public Action _preflightAction;
+
+	public bool IsTargeting => _preflightAction != null;
 
 	public override void _EnterTree() {
 		instance = this;
@@ -30,36 +38,10 @@ public partial class ComponentActionMenu : Control {
 		return true;
 	}
 
-	public override void _Input(InputEvent @event) {
-		if (@event.IsActionPressed("Battle Confirm")) {
-			if (_preflightAction != null) {
-				_SubmitAction();
-
-				return;
-			}
-
-			_BeginTargetSelect(Action.ActionTypes.Attack);
-		}
-	}
-
 	public void UntargetAllActors() {
 		foreach (var actor in ControllerActors.instance.Actors) {
 			actor.Untarget();
 		}
-	}
-
-	public void _BeginTargetSelect(Action.ActionTypes actionType) {
-		_attackIcon.Hide();
-		_inputAudio.Play();
-
-		UntargetAllActors();
-
-		// @TODO Load last confirmed enemy list, if available.
-		ControllerActors.instance.Enemies.First().Target();
-
-		_preflightAction = new Action {
-			actionType = actionType
-		};
 	}
 
 	public void _SubmitAction() {
@@ -76,5 +58,93 @@ public partial class ComponentActionMenu : Control {
 		ControllerAtb.instance.Enqueue(_actor);
 
 		// @TODO Save last confirmed enemy list.
+	}
+
+	public void _TryConfirm(InputEvent @event) {
+		if (!@event.IsActionPressed("Confirm")) {
+			return;
+		}
+
+		if (_preflightAction != null) {
+			_SubmitAction();
+
+			return;
+		}
+
+		_BeginTargetSelect(Action.ActionTypes.Attack);
+	}
+
+	public bool _TrySelect(InputEvent @event) {
+		if (!IsTargeting) {
+			return false;
+		}
+
+		if (@event.IsActionPressed("East")) {
+			_SelectNextTarget(Direction.Right);
+		}
+		else if (@event.IsActionPressed("West")) {
+			_SelectNextTarget(Direction.Left);
+		}
+
+		return true;
+	}
+
+	public void _BeginTargetSelect(Action.ActionTypes actionType) {
+		_attackIcon.Hide();
+		_inputAudio.Play();
+
+		UntargetAllActors();
+
+		// @TODO Load last confirmed enemy list, if available.
+		ControllerActors.instance.Enemies.First().Target();
+
+		_preflightAction = new Action {
+			actionType = actionType
+		};
+	}
+
+	public void _SelectNextTarget(Direction direction) {
+		var enemies = ControllerActors.instance.Enemies.ToList();
+		if (enemies.Count == 0) {
+			return;
+		}
+
+		var currentTarget = enemies.FirstOrDefault(actor => actor.IsTargeted);
+		var currentPosition = currentTarget.Position;
+
+		var nextTarget = (ActorBase)null;
+		var closestDistance = float.MaxValue;
+
+		foreach (var enemy in enemies) {
+			var isDesiredDirection = direction switch {
+				Direction.Right => enemy.Position.X > currentPosition.X,
+				Direction.Left => enemy.Position.X < currentPosition.X,
+				_ => false
+			};
+
+			if (!isDesiredDirection) {
+				continue;
+			}
+
+			var distance = currentPosition.DistanceTo(enemy.Position);
+			if (distance > closestDistance) {
+				continue;
+			}
+
+			closestDistance = distance;
+			nextTarget = enemy;
+		}
+
+		if (nextTarget == null) {
+			return;
+		}
+
+		UntargetAllActors();
+		nextTarget.Target();
+	}
+
+	public override void _Input(InputEvent @event) {
+		_TryConfirm(@event);
+		_TrySelect(@event);
 	}
 }
